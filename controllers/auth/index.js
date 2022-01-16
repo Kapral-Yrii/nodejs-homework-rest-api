0d10f4aba24dc5139fc3efd7fb43536a07f068d1
Kapral-Yrii <kapral2701@gmail.com>
2022-01-16 21:38:52 +0200

    complite task

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,16 +1,18 @@
 import { HttpCode } from '../../lib/constants'
-import AuthService from '../../service/auth'
-
-const authService = new AuthService()
+import authService from '../../service/auth'
 
 const signup = async (req, res, next) => {
-    const { email } = req.body
-    const isUserExist = await authService.isUserExist(email)
-    if (isUserExist) {
-        return res.status(HttpCode.CONFLICT).json({ status: 'error', code: HttpCode.CONFLICT, message: 'Email is a already exist', })
+    try {
+        const { email } = req.body
+        const isUserExist = await authService.isUserExist(email)
+        if (isUserExist) {
+            return res.status(HttpCode.CONFLICT).json({ status: 'error', code: HttpCode.CONFLICT, message: 'Email is a already exist', })
+        }
+        const data = await authService.create(req.body)
+        res.status(HttpCode.CREATED).json({ status: 'success', code: HttpCode.CREATED, data })
+    } catch (error) {
+        next(error)
     }
-    const data = await authService.create(req.body)
-    res.status(HttpCode.CREATED).json({ status: 'success', code: HttpCode.CREATED, data })
 }
 
 const login = async (req, res, next) => {
